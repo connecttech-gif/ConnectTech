7a11b0645f45f90eec7de5b34ee8d5e0e0a46f64
connecttech-gif <limajjfer@gmail.com>
2026-07-03 16:36:56 -0300

    Criado arquivo de configuração do Supabase

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// ===== CONEXÃO COM O SUPABASE =====
+const SUPABASE_URL = 'https://xwsismsopaujzgnwdgrv.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_vbEzC-ZuvpEppUlqpdJjQQ_PTUBKhJc';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
